0b7f71bc42de34d5f681e742684d3a2ecf4668ad
tjthechemist <mr.tawatchai@live.com>
2025-11-02 22:04:26 +0700

    Switch between value and fix button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import axios from "axios";
 import './App.css';
 import { Input } from './components/ui/input';
-import { Button } from './components/ui/button';
 
 function App() {
   const [math1, setMath1] = useState("");
@@ -32,14 +31,14 @@ function App() {
           <Input
             type="number"
             placeholder="Module 1"
-            value={math1}
+            value={math2}
             onChange={(e) => setMath1(e.target.value)}
             className="border p-2 rounded m-1"
           />
           <Input
             type="number"
             placeholder="Module 2"
-            value={math2}
+            value={math1}
             onChange={(e) => setMath2(e.target.value)}
             className="border p-2 rounded m-1"
           />
@@ -50,25 +49,25 @@ function App() {
           <Input
             type="number"
             placeholder="Module 1"
-            value={verbal1}
+            value={verbal2}
             onChange={(e) => setVerbal1(e.target.value)}
             className="border p-2 rounded m-1"
           />
           <Input
             type="number"
             placeholder="Module 2"
-            value={verbal2}
+            value={verbal1}
             onChange={(e) => setVerbal2(e.target.value)}
             className="border p-2 rounded m-1"
           />
         </div>
 
-        <Button
+        <button
           className="bg-blue-400 text-white px-4 py-2 rounded-xl"
           onClick={handleCheck}
         >
           Calculate Total
-        </Button>
+        </button>
 
         {total && !total.error && (
           <div className="text-xl mt-4">
