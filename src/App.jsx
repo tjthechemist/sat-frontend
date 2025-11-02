b08f0b2561a7c786c29ff5bd03018e36f43e3ec2
tjthechemist <mr.tawatchai@live.com>
2025-11-02 21:22:39 +0700

    fix button color

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
         </div>
 
         <Button
-          className="bg-blue-600 text-white px-4 py-2 rounded-xl"
+          className="bg-blue-400 text-white px-4 py-2 rounded-xl"
           onClick={handleCheck}
         >
           Calculate Total
