b410d81bccb97d87ccaf464d43c987c6c1d4ae3c
tjthechemist <mr.tawatchai@live.com>
2025-11-03 11:50:58 +0700

    add light mode

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
         </div>
 
         <Button
-          className="bg-blue-600 text-white px-4 py-2 rounded-xl"
+          className="dark:bg-blue-600 dark:text-white px-4 py-2 rounded-xl text-blue-600 border-blue-600"
           onClick={handleCheck}
         >
           Calculate Total
